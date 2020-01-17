2deefcca7c0feacab177bd5b73b9f465bc60ddee
rishabhdavesar <32301957+rishabhdavesar@users.noreply.github.com>
2020-01-17 19:18:33 +0530

    animation done

--- lib/screen/clock.dart
+++ lib/screen/clock.dart
@@ -24,52 +24,60 @@ double height = MediaQuery.of(context).size.height;
            
           },
         ),
-        backgroundColor: 
+        
+        body: AnimatedContainer(
+            duration: Duration(milliseconds:600 ),
+            // Provide an optional curve to make the animation feel smoother.
+          color: 
         value?Colors.grey[200]:Colors.grey[850],
-        body: Center(
-          child: Container(
-            //height: height/2,
-            width: width/2,
-           // padding: EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              //  border: Border.all(width: 2.0, color: Colors.black),
-                color: value?Colors.grey[300]:Colors.grey[850],
-                //sborderRadius: BorderRadius.all(Radius.circular(50)),
-                shape: BoxShape.circle,
-                
-              
-                ),
-            child: AnalogClock(
-              hourHandColor: value?Colors.black:Colors.white,
-              minuteHandColor: value?Colors.black:Colors.white,
-              
+            curve: Curves.linearToEaseOut,
+                  child: Center(
+            child: AnimatedContainer(
+               duration: Duration(seconds: 1),
+              //height: height/2,
+              width: width/2,
+             // padding: EdgeInsets.all(5),
               decoration: BoxDecoration(
-                  boxShadow: [
-                  BoxShadow(
-                     color: value?Colors.grey[500]:Colors.grey[900],
-                     offset: Offset(5, 5),
-                     blurRadius: 15,
-                     spreadRadius: 1,
-
-
+                //  border: Border.all(width: 2.0, color: Colors.black),
+                  color: value?Colors.grey[300]:Colors.grey[850],
+                  //sborderRadius: BorderRadius.all(Radius.circular(50)),
+                  shape: BoxShape.circle,
+                  
+                
                   ),
-                  BoxShadow(
-                    color: value?Colors.white:Colors.grey[800],
-                    offset: Offset(-5,-5),
-                    blurRadius: 12,
-                    spreadRadius: 1
-                  )
-                 
+              child: AnalogClock(
+                hourHandColor: value?Colors.black:Colors.white,
+                minuteHandColor: value?Colors.black:Colors.white,
+                
+                decoration: BoxDecoration(
+                    boxShadow: [
+                    BoxShadow(
+                       color: value?Colors.grey[500]:Colors.grey[900],
+                       offset: Offset(5, 5),
+                       blurRadius: 15,
+                       spreadRadius: 1,
 
-                ],
-                  //border: Border.all(width: 2.0, color: Colors.black),
-                  color: value?Colors.white:Colors.grey[850],
-                  shape: BoxShape.circle),
-              width: 150.0,
-              isLive: true,
-              showNumbers: false,
-        showDigitalClock: false,
-              datetime: DateTime(2019, 1, 1, 9, 12, 15),
+
+                    ),
+                    BoxShadow(
+                      color: value?Colors.white:Colors.grey[800],
+                      offset: Offset(-5,-5),
+                      blurRadius: 12,
+                      spreadRadius: 1
+                    )
+                   
+
+                  ],
+                    //border: Border.all(width: 2.0, color: Colors.black),
+                    color: value?Colors.white:Colors.grey[850],
+                    
+                    shape: BoxShape.circle),
+                width: 150.0,
+                isLive: true,
+                showNumbers: false,
+          showDigitalClock: false,
+                datetime: DateTime(2019, 1, 1, 9, 12, 15),
+              ),
             ),
           ),
         ),
@@ -77,4 +85,3 @@ double height = MediaQuery.of(context).size.height;
     );
   }
 }
-
